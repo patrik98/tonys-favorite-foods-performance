45aba5378d8017a837bfcf9d73711b1e61f54f1a
Patrik Golec <2010738979@stud.fh-kufstein.ac.at>
2021-03-20 15:42:58 +0100

    implemented image resizing in build script

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,3 @@ window.addEventListener('load', () => {
   document.querySelector('header > h1').textContent = "Tony's Favorite Foods";
   document.querySelector('footer').textContent = "Built with 🐟 by Tony the 🐈";
 });
-
--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,28 @@
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 
 function asset(filename) {
-    return path.join(__dirname + "/../assets", filename);
+    return path.join(__dirname + '/../assets', filename);
 }
 
 function target(filename) {
-    return path.join(__dirname + "/../www/assets", filename);
+    return path.join(__dirname + '/../www/assets', filename);
 }
 
-fs.copyFileSync(asset('images/soop.jpg'), target('images/soop.jpg'));
-fs.copyFileSync(asset('images/brrto.jpg'), target('images/brrto.jpg'));
-fs.copyFileSync(asset('images/fish.jpg'), target('images/fish.jpg'));
-fs.copyFileSync(asset('images/pezza.jpg'), target('images/pezza.jpg'));
+async function resizeAndMove(img) {
+    await sharp(asset(img))
+    .resize(null, 200)
+    .jpeg()
+    .toFile(target(img))
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+resizeAndMove('images/soop.jpg');
+resizeAndMove('images/brrto.jpg');
+resizeAndMove('images/fish.jpg');
+resizeAndMove('images/pezza.jpg');
 fs.copyFileSync(asset('css/style.css'), target('css/style.css'));
 fs.copyFileSync(asset('js/index.js'), target('js/index.js'));
